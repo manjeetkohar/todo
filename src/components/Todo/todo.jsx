44d917f36c3494f0440b98f899f0377c251f82dd
manjeetkohar <manjeetarya709@gmail.com>
2022-11-10 14:14:46 -0500

    final commit

--- src/components/Todo/todo.jsx
+++ src/components/Todo/todo.jsx
@@ -2,10 +2,14 @@ import styles from "./todo.module.css"
 const Todo =({todo, index, markTodo, deleteTodo})=>{
     if (todo.text ==="" ||todo.text ===null ||todo.text === undefined) return null;
     return(
-        <div className={styles.todo_list_panel}>
+        <div className={styles.main}>
+            <div className={styles.todo_list_panel}>
             <span className={styles.todo} style={{textDecoration:todo.isDone? "line-through" :""}}>{todo.text}</span>
-            <button onClick={()=>markTodo(index)}>✓</button>
-            <button onClick={()=>deleteTodo(index)}>✕</button>
+        </div>
+        <div>
+            <button className={styles.todo_done} onClick={()=>markTodo(index)}>✓</button>
+            <button className={styles.todo_delete} onClick={()=>deleteTodo(index)}>✕</button>
+        </div>
         </div>
     )
 }
